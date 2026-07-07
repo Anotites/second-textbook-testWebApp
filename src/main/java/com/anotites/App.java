50632f044db773e091d75ee576722348ca3a7890
Anotites <anotites@gmail.com>
2026-07-07 19:47:06 +0300

    Initial commit for testWebApp

---
+++ src/main/java/com/anotites/App.java
@@ -0,0 +1,13 @@
+package com.anotites;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
